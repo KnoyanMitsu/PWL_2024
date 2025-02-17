1b02f4a8de802d67e5d477cb12353b8988a6e030
= <=>
2025-02-17 03:19:15 +0000

    Pratikum 1 Jobsheet 2

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,29 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function () {
+    return '244107023004 Fabian Ananda Merdana';
+});
+
+Route::get('/hello', function(){
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/user/{name?}', function ($name='John'){
+    return 'Nama saya '. $name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function($postId, $commentId) {
+        return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{article}', function($article){
+    return 'Halaman Artikel dengan ID '. $article;
 });
